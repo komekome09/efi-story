924326e5abaff5871db5b1ce24a4fd8987235236
komekome09 <fanmaskome@gmail.com>
2019-06-16 15:36:02 +0900

    Change method of graphics from set buffer which output target to overall of display

--- src/main.c
+++ src/main.c
@@ -27,44 +27,53 @@ EFI_STATUS efi_main(EFI_HANDLE ImgHandle, EFI_SYSTEM_TABLE *SysTable){
 	LibLocateProtocol(&EfiGraphicsOutputProtocolGuid, (void **)&GraphicsOutput);
 	LibLocateProtocol(&EfiPciIoProtocolGuid, (void **)&PciIo);
     
-    Print(L"%d\n", GraphicsOutput->Mode->Info->HorizontalResolution);
-    Print(L"%d\n", GraphicsOutput->Mode->Info->VerticalResolution);
+    Print(L"x = %d\n", GraphicsOutput->Mode->Info->HorizontalResolution);
+    Print(L"y = %d\n", GraphicsOutput->Mode->Info->VerticalResolution);
 
-	VOID	*BmpBuffer = NULL;
-	UINTN	BmpSize;
-	Status = LoadImageFile(ImgHandle,  FileName[0], &BmpBuffer, &BmpSize);
+	VOID	*ImgBuffer = NULL;
+	UINTN	ImgSize;
+	Status = LoadFile(ImgHandle,  FileName[0], &ImgBuffer, &ImgSize);
 	if(EFI_ERROR(Status)){
-		if(BmpBuffer != NULL){
-			FreePool(BmpBuffer);
+		if(ImgBuffer != NULL){
+			FreePool(ImgBuffer);
 		}
 		Print(L"Load Image failed.\n");
 		return Status;
 	}
 
-	Status = DrawImage(GraphicsOutput, BmpBuffer, BmpSize, 32, 64 + 16*51, 16, 16, 300, 0);
-    CHAR16 apart = L'㌀';
-    Print(L"%x\n", apart);
-	if(EFI_ERROR(Status)){
-		Print(L"Draw bmp failed.\n");
-		return Status;
+    INT32 Width = 0, Height = 0, Bpp = 0;
+    UINT8 *Pixels = stbi_load_from_memory((UINT8*)ImgBuffer, ImgSize, &Width, &Height, &Bpp, 0);
+    if(Pixels == NULL){
+        Print(L"%s\n", (CHAR16*)stbi_failure_reason());
+        return EFI_INVALID_PARAMETER;
+    }
+
+    INT32 x = 300;
+    while(1){
+        Status = DrawImage(GraphicsOutput, Pixels, Width, Height, Bpp, 32, 64 + 16*51, 16, 16, x, 0);
+        CHAR16 apart = L'㌀';
+        //Print(L"%x\n", apart);
+        if(EFI_ERROR(Status)){
+            Print(L"Draw bmp failed.\n");
+            return Status;
+        }
+        
+        x+=2;
+        if(x + 32 > GraphicsOutput->Mode->Info->HorizontalResolution) x=0;
+    }
+
+	if(ImgBuffer != NULL){
+		FreePool(ImgBuffer);
 	}
 
-	Status = DrawImage(GraphicsOutput, BmpBuffer, BmpSize, 100, 100, 300, 300, 300, 300);
-	if(EFI_ERROR(Status)){
-		Print(L"Draw bmp failed.\n");
-		return Status;
-	}
-
-	if(BmpBuffer != NULL){
-		FreePool(BmpBuffer);
-	}
+    StatusToString(NULL, EFI_SUCCESS);
 
     while(1);
 
     return EFI_SUCCESS;
 }
 
-STATIC EFI_STATUS LoadImageFile(IN EFI_HANDLE Handle, IN CHAR16 *Path,	OUT	void **BmpBuffer, OUT UINTN *BmpSize){
+STATIC EFI_STATUS LoadFile(IN EFI_HANDLE Handle, IN CHAR16 *Path,	OUT	void **FileBuffer, OUT UINTN *FileSize){
 	EFI_STATUS					Status = EFI_SUCCESS;
 	EFI_FILE_IO_INTERFACE		*SimpleFile;
     EFI_GUID					SimpleFileSystemProtocolGuid = SIMPLE_FILE_SYSTEM_PROTOCOL;
@@ -97,53 +106,46 @@ STATIC EFI_STATUS LoadImageFile(IN EFI_HANDLE Handle, IN CHAR16 *Path,	OUT	void 
 		return EFI_OUT_OF_RESOURCES;
 	}
 	Status = uefi_call_wrapper(File->Read, 3, File, &BufferSize, Buffer);
-	if(BufferSize == MAX_BUFFER_SIZE){
-		Print(L"BufferSize: %r\n", EFI_OUT_OF_RESOURCES);
+	if(EFI_ERROR(Status)){
+        StatusToString(NULL, Status);
 		if(Buffer != NULL){
 			FreePool(Buffer);
 		}
-		return EFI_OUT_OF_RESOURCES;
+		return Status;
 	}
 
 	Buffer = ReallocatePool(Buffer, MAX_BUFFER_SIZE, BufferSize);
 
-	*BmpBuffer = Buffer;
-	*BmpSize   = BufferSize;
+	*FileBuffer = Buffer;
+	*FileSize   = BufferSize;
 
 	return Status;
 }
 
-STATIC EFI_STATUS DrawImage(IN EFI_GRAPHICS_OUTPUT_PROTOCOL *GraphicsOutput, 
-                            IN void *ImgBuffer, IN UINTN ImgSize, 
+STATIC EFI_STATUS DrawImage(IN EFI_GRAPHICS_OUTPUT_PROTOCOL *GraphicsOutput, IN void *Pixels, 
+                            IN INT32 Width, IN INT32 Height, IN INT32 Bpp,
                             IN INT32 XLShow, IN INT32 YTShow, IN INT32 XRShow, IN INT32 YBShow,
                             IN INT32 x, IN INT32 y){
     EFI_STATUS      Status = EFI_SUCCESS;
-    UINT8           *Pixels;
-    INT32           Width = 0;
-    INT32           Height = 0;
-    INT32           Bpp = 0;
 
-    if(ImgBuffer == NULL){
-        return EFI_INVALID_PARAMETER;
-    }
-
-    Pixels = stbi_load_from_memory((UINT8*)ImgBuffer, ImgSize, &Width, &Height, &Bpp, 0);
     if(Pixels == NULL){
-        Print(L"%s\n", (CHAR16*)stbi_failure_reason());
         return EFI_INVALID_PARAMETER;
     }
 
-    Print(L"Width       = %d\n", Width);
-    Print(L"Height      = %d\n", Height);
-    Print(L"Bpp         = %d\n", Bpp);
+    //Print(L"Width       = %d\n", Width);
+    //Print(L"Height      = %d\n", Height);
+    //Print(L"Bpp         = %d\n", Bpp);
 
     EFI_GRAPHICS_OUTPUT_BLT_PIXEL   *BltBuffer;
     UINT8                           *ImgIndex = Pixels;
     INTN                            BltPos = 0;
+    INT32                           DispWidth = GraphicsOutput->Mode->Info->HorizontalResolution;
+    INT32                           DispHeight = GraphicsOutput->Mode->Info->VerticalResolution;
+    UINTN                           AllocateSize = sizeof(EFI_GRAPHICS_OUTPUT_BLT_PIXEL) * DispWidth * DispHeight;
 
-    BltBuffer = AllocateZeroPool(sizeof(EFI_GRAPHICS_OUTPUT_BLT_PIXEL) * XRShow * YBShow); //(XRShow - XLShow) * (YBShow - YTShow));
+    BltBuffer = AllocateZeroPool(AllocateSize); 
     if(BltBuffer == NULL){
-		Print(L"BltBuffer: %r\nSize = %d\n", EFI_OUT_OF_RESOURCES, ImgSize);
+		Print(L"BltBuffer: %r\nSize = %d\n", EFI_OUT_OF_RESOURCES, AllocateSize);
 		return EFI_OUT_OF_RESOURCES;
     }
 
@@ -152,20 +154,29 @@ STATIC EFI_STATUS DrawImage(IN EFI_GRAPHICS_OUTPUT_PROTOCOL *GraphicsOutput,
     if(YTShow < 0) YTShow = 0;
     if(YTShow + YBShow > Height) YBShow = Height - YTShow;
 
-    Print(L"XLShow      = %d\n", XLShow);
-    Print(L"XRShow      = %d\n", XRShow);
-    Print(L"YTShow      = %d\n", YTShow);
-    Print(L"YBShow      = %d\n", YBShow);
+    //Print(L"XLShow      = %d\n", XLShow);
+    //Print(L"XRShow      = %d\n", XRShow);
+    //Print(L"YTShow      = %d\n", YTShow);
+    //Print(L"YBShow      = %d\n", YBShow);
+    //Print(L"x           = %d\n", x     );
+    //Print(L"y           = %d\n", y     );
 
-    for(INTN YIndex = YTShow; YIndex < YTShow + YBShow; YIndex++){
-        if(YTShow > 0 && YIndex == YTShow){
+    for(INTN index = 0; index < DispWidth * DispHeight; index++){
+        BltBuffer[index].Red = 0x00; 
+        BltBuffer[index].Green = 0x00; 
+        BltBuffer[index].Blue = 0x00; 
+        BltBuffer[index].Reserved = 0x00; 
+    }
+
+    for(INTN YIndex = y; YIndex < y + YBShow; YIndex++){
+        if(YTShow > 0 && YIndex == y){
             ImgIndex += Width * YTShow * Bpp;
         }
-        for(INTN XIndex = XLShow; XIndex < XLShow + XRShow; XIndex++){
-            if(XLShow > 0 && XIndex == XLShow){
+        for(INTN XIndex = x; XIndex < x + XRShow; XIndex++){
+            if(XLShow > 0 && XIndex == x){
                 ImgIndex += XLShow*Bpp;
             }
-			BltPos = YIndex * XRShow + XIndex - XLShow;
+			BltPos = YIndex * DispWidth + XIndex;
 			switch(Bpp){
 				case 4:					
                     BltBuffer[BltPos].Red           = *ImgIndex++;
@@ -193,11 +204,15 @@ STATIC EFI_STATUS DrawImage(IN EFI_GRAPHICS_OUTPUT_PROTOCOL *GraphicsOutput,
         }
 	}
 
-	Status = uefi_call_wrapper(GraphicsOutput->Blt, 10, GraphicsOutput, BltBuffer, EfiBltBufferToVideo, XLShow, YTShow, x, y, XRShow, YBShow, XRShow * sizeof(EFI_GRAPHICS_OUTPUT_BLT_PIXEL));
+	Status = uefi_call_wrapper(GraphicsOutput->Blt, 10, GraphicsOutput, BltBuffer, EfiBltBufferToVideo, 0, 0, 0, 0, DispWidth, DispHeight, DispWidth * sizeof(EFI_GRAPHICS_OUTPUT_BLT_PIXEL));
 	if(EFI_ERROR(Status)){
 		Print(L"Blt: %x\n", Status);
 		return Status;
 	}
+
+    if(BltBuffer != NULL){
+        FreePool(BltBuffer);
+    }
 
     return Status;
 }
